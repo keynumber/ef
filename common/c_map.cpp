c9d5abdf875da164cb82b6cdd3755475eca58ddb
keynumber <keynumber@yeah.net>
2015-11-15 17:14:09 +0800

    add configure map

--- common/c_map.cpp
+++ common/c_map.cpp
@@ -6,14 +6,16 @@
 #include "c_map.h"
 
 #include <cassert>
+#include <cstdio>
 #include <fstream>
 
 #include "string_util.h"
 
 namespace ef {
 
-static const char kCMapIdentifier[] = "{}[]=";
+const Value kNullValue(kValueNull);
 
+static const char kCMapIdentifier[] = "{}[]=";
 
 CMap::CMap()
 {
@@ -34,7 +36,7 @@ bool CMap::Load(const char * path)
     std::stack<CMapItemType> type_st;
     std::stack<void *> content_pst;
 
-    type_st.push(kCMapItemGlobal);
+    type_st.push(kValueGlobal);
     content_pst.push(static_cast<void*>(&_content));
 
     int line = 0;
@@ -47,7 +49,7 @@ bool CMap::Load(const char * path)
         // 删除注释
         std::string::size_type pos = str.find('#');
         str = str.substr(0, pos);
-        str = trim("\r\n\t ");
+        str = trim(str);
 
         if(str == "") {
             continue;
@@ -59,6 +61,11 @@ bool CMap::Load(const char * path)
     }
 
     in.close();
+
+    if (type_st.size() != 1 || content_pst.size() != 1) {
+        _errmsg = "wrong format, uncomplete file";
+        return false;
+    }
     return true;
 }
 
@@ -68,81 +75,199 @@ bool CMap::HandleContent(const std::string &str, int line,
 {
     std::string::size_type pos = str.find_first_of(kCMapIdentifier);
     if (pos == std::string::npos) {
-        _errmsg = "format error at line " + std::to_string(line) + str;
+        _errmsg = "format error at line " + std::to_string(line) + ": " + str;
         return false;
     }
 
     CMapItemType type = tst->top();
-    void *p = pst->top();
+    void * cur_content = pst->top();
 
     switch (str[pos]) {
-    case '=': {
+    case '=':
+    {
         std::vector<std::string> sp;
-        split(str, "\t\r\n =", &sp);
+        splitany(str, "\t\r\n\r =", &sp);
         if (sp.size() != 2) {
-            _errmsg = "format error at line " + std::to_string(line) + str;
+            _errmsg = "format error at line " + std::to_string(line) + ": " + str;
             return false;
         }
 
         const std::string &key = sp[0];
         const std::string &value= sp[1];
 
-        if (type == kCMapItemGlobal) {
-            Value val;
-            val.type = kCMapItemGlobal;
-            val.value.str = value;
+        if (type == kValueGlobal)
+        {
+            CMapContent &content = *(static_cast<CMapContent*>(cur_content));
+            CMapContent::iterator it = content.find(key);
+            if (it != content.end()) {
+                content.erase(it);
+            }
 
-            CMapContent &content = *(static_cast<CMapContent*>(p));
-            content[key] = val;
-        } else if (type == kCMapItemObject) {
-            Object & obj =  *(static_cast<Object*>(p));
-            obj[key] = value;
-        } else {
+            Value &val = content[key];
+            val.type = kValueString;
+            val.value = static_cast<void*>(new std::string(value));
+        }
+        else if (type == kValueObject)
+        {
+            Object & obj =  *static_cast<Object*>(cur_content);
+            Object::iterator it = obj.find(key);
+            if (it != obj.end()) {
+                obj.erase(it);
+            }
+
+            Value &val = obj[key];
+            val.type = kValueString;
+            val.value = static_cast<void*>(new std::string(value));
+        }
+        else
+        {
             assert(false);
         }
 
         break;
     }
 
-    case '{': {
-
+    case '{':
+    {
         void *p = nullptr;
         std::string key = trim(str.substr(0, pos));
-        if (type == kCMapItemGlobal) {
-            CMapContent * content = static_cast<CMapContent*>(p);
-            p = static_cast<void*>(&content[key]);
-        } else if (type == kCMapItemArray) {        // TODO compile error
-            Array * arr =  static_cast<Array*>(p);
-            arr->push_back(Object());
-            p = static_cast<void*>(&arr->back());
-        } else {
+
+
+        if (type == kValueGlobal)
+        {
+            CMapContent & content = *static_cast<CMapContent*>(cur_content);
+            // 为了得到val.value,因而不能使用拷贝构造函数,通过获取在map中的引用,得到val.value
+            CMapContent::iterator it = content.find(key);
+            if (it != content.end()) {
+                content.erase(it);
+            }
+
+            Value &val = content[key];
+            val.type = kValueObject;
+            val.value = static_cast<void*>(new Object);
+            p = val.value;
+        }
+        else if (type == kValueArray)
+        {
+            Array & arr =  *static_cast<Array*>(cur_content);
+            Value v;
+            arr.push_back(v);
+            Value &val = arr.back();
+            val.type = kValueObject;
+            val.value = static_cast<void*>(new Object);
+            p = val.value;
+        }
+        else
+        {
             assert(false);
         }
 
-        tst->push(kCMapItemObject);
+        tst->push(kValueObject);
         pst->push(p);
         break;
     }
 
-    case '}': {
+    case '}':
+    {
+        pst->pop();
+        tst->pop();
         break;
     }
 
-    case '[': {
+    case '[':
+    {
+        void *p = nullptr;
+        std::string key = trim(str.substr(0, pos));
+
+        if (type == kValueGlobal) {
+            CMapContent & content = *static_cast<CMapContent*>(cur_content);
+            CMapContent::iterator it = content.find(key);
+            if (it != content.end()) {
+                content.erase(it);
+            }
+
+            Value &val = content[key];
+            val.type = kValueArray;
+            val.value = static_cast<void*>(new Array);
+            p = val.value;
+        } else {
+            assert(false);
+        }
+
+        tst->push(kValueArray);
+        pst->push(p);
         break;
     }
 
-    case ']': {
+    case ']':
+    {
+        pst->pop();
+        tst->pop();
         break;
     }
 
-    default: {
+    default:
+    {
         assert(false);
         break;
     }
+
+    }
+    return true;
+}
+
+void CMap::Print() const
+{
+    for (const auto & it : _content) {
+        if (it.second.type == kValueString)
+        {
+            const std::string & second = *static_cast<std::string*>(it.second.value);
+            printf("%s = %s\n", it.first.c_str(), second.c_str());
+        }
+        else if (it.second.type == kValueObject)
+        {
+            printf("%s {\n", it.first.c_str());
+            // 这里其实应该做类型检查的||-_-..........
+            // 还是有时间再做吧.....
+            const Object & object = *static_cast<Object*>(it.second.value);
+            for (const auto &item : object)
+            {
+                const std::string & second = *static_cast<std::string*>(item.second.value);
+                printf("\t%s = %s\n", item.first.c_str(), second.c_str());
+            }
+            printf("}\n");
+
+        }
+        else if (it.second.type == kValueArray)
+        {
+            int index = 0;
+            printf("%s [\n", it.first.c_str());
+            const Array & array = *static_cast<Array*>(it.second.value);
+            for (const auto &ait : array)
+            {
+                printf("\t%d {\n", index++);
+                const Object & object = *static_cast<Object*>(ait.value);
+                for (const auto & item : object)
+                {
+                    const std::string & second = *static_cast<std::string*>(item.second.value);
+                    printf("\t\t%s = %s\n", item.first.c_str(), second.c_str());
+                }
+                printf("\t}\n");
+            }
+            printf("]\n");
+        }
     }
 }
 
+const Value & CMap::operator[] (const std::string key) const
+{
+    CMapContent::const_iterator it = _content.find(key);
+    if (it == _content.end()) {
+        return kNullValue;
+    }
+
+    return it->second;
+}
 const std::string & CMap::GetErrMsg() const
 {
     return _errmsg;
@@ -150,10 +275,29 @@ const std::string & CMap::GetErrMsg() const
 
 } /* namespace ef */
 
-#if 0
+#if 1
 
-int main()
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        printf("Usage: %s <load file name>\n", argv[0]);
+        return -1;
+    }
+
+    ef::CMap map;
+    if (!map.Load(argv[1])) {
+        printf("error: %s\n", map.GetErrMsg().c_str());
+        return -1;
+    }
+
+    cout << "map[key]:\t" << map["key"].str() << endl;
+    cout << "map[server][port]:\t" << map["server"]["port"].str() << endl;
+    cout << "map[mysql][0][port]:\t" << map["mysql"][0]["port"].str() << endl << endl;
+
+    map.Print();
     return 0;
 }
 
--- common/c_map.h
+++ common/c_map.h
@@ -13,51 +13,124 @@
 
 namespace ef {
 
-class CMap {
-private:
-    enum CMapItemType {
-        kCMapItemGlobal,
-        kCMapItemObject,
-        kCMapItemArray
-    };
+enum CMapItemType {
+    kValueNull,   // use internal
+    kValueGlobal,   // use internal
+    kValueString,
+    kValueObject,
+    kValueArray,
+};
 
-    typedef std::string String;
-    typedef std::unordered_map<std::string, String> Object;
-    typedef std::vector<Object> Array;
-    union Storage {
-        Storage() {}
-        ~Storage() {}
+struct Value;
+typedef std::unordered_map<std::string, Value> Object;
+typedef std::vector<Value> Array;
+typedef std::unordered_map<std::string, Value> CMapContent;
 
-        String str;
-        Object obj;
-        Array arr;
-    };
+extern const Value kNullValue;
 
-    struct Value {
-        Value () {}
-        virtual ~Value () {}
-        Value & operator= (const Value & v)
-        {
-            type = v.type;
-            if (type  == kCMapItemGlobal) {
-                value.str = v.value.str;
-            } else if (type == kCMapItemArray) {
-                value.arr = v.value.arr;
-            } else if (type == kCMapItemObject) {
-                value.obj = v.value.obj;
-            }
+struct Value {
+    Value () : type(kValueNull), value(nullptr) {};
+    Value (CMapItemType t) : type(t), value(nullptr) {};
+    Value(const Value &val) { Copy(val); }
+    Value & operator=(const Value &val) { Copy(val); }
+
+    virtual ~Value ()
+    {
+        Destory();
+    }
+
+    void Destory() {
+        if (type  == kValueString) {
+            std::string *str = static_cast<std::string*>(value);
+            delete str;
+        } else if (type == kValueArray) {
+            Array *arr = static_cast<Array*>(value);
+            delete arr;
+        } else if (type == kValueObject) {
+            Object *obj = static_cast<Object*>(value);
+            delete obj;
         }
 
-        Storage value;
-        CMapItemType type;
-    };
-    typedef std::unordered_map<std::string, Value> CMapContent;
+        value  = nullptr;
+    }
 
+    void Copy(const Value& val) {
+        // 如果有数据,先删除
+        if (value) {
+            Destory();
+        }
+
+        // 少一个type赋值导致的大坑,使用vecotr保存的对象,因为内存需要连续,因此每次push的时候
+        // 有时候会复制整个vector中的每个对象,因而type混乱,导致type全乱
+        // 做好类型检查很重要啊
+        type = val.type;
+        if (val.value) {
+            if (type  == kValueString) {
+                std::string *str = static_cast<std::string*>(val.value);
+                value = static_cast<void*>(new std::string(*str));
+            } else if (type == kValueArray) {
+                Array *arr = static_cast<Array*>(val.value);
+                value = static_cast<void*>(new Array(*arr));
+            } else if (type == kValueObject) {
+                Object *obj = static_cast<Object*>(val.value);
+                value = static_cast<void*>(new Object(*obj));
+            }
+        }
+    }
+
+    const std::string str() const {
+        if (type != kValueString) {
+            return "";
+        }
+        return *static_cast<std::string*>(value);
+    }
+
+    const Value & operator[] (const std::string key) const
+    {
+        if (type != kValueObject) {
+            return kNullValue;
+        }
+
+        const Object *obj = static_cast<Object*>(value);
+        if (!obj) {
+            return kNullValue;
+        }
+
+        Object::const_iterator it = obj->find(key);
+        if (it == obj->end()) {
+            return kNullValue;
+        }
+
+        return it->second;
+    }
+
+    const Value & operator[] (int index) const
+    {
+        if (type != kValueArray) {
+            return kNullValue;
+        }
+
+        Array * arr = static_cast<Array*>(value);
+        if (!arr || arr->size() <= index) {
+            return kNullValue;
+        }
+
+        return (*arr)[index];
+    }
+
+    void *value;
+    CMapItemType type;
+};
+
+
+class CMap {
 public:
     CMap();
     virtual ~CMap();
     bool Load(const char * path);
     const std::string & GetErrMsg() const;
+    void Print() const;
+    const Value & operator[] (const std::string key) const;
 
 private:
     bool HandleContent(const std::string &str, int line,
