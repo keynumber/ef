6dfedcbec6f0995ddc9c217c2b6782ee1ed5e781
keynumber <keynumber@yeah.net>
2015-11-19 00:40:09 +0800

    add socket buffer

---
+++ common/socket_buffer.cpp
@@ -0,0 +1,324 @@
+/*
+ * Author: number
+ * Date  : Nov 18, 2015
+ */
+
+#include "socket_buffer.h"
+
+#include <cassert>
+#include <cstring>
+
+#include <unistd.h>
+
+#include "mem_pool.h"
+
+namespace ef {
+
+SocketBuffer::SocketBuffer(MemPool *pool, uint32_t block_size/* = DEFAULT_BUFFER_BLOCK_SIZE */)
+    : _block_size(block_size)
+    , _buf_size(0)
+    , _mem_pool(pool)
+{
+    assert(_mem_pool);
+}
+
+SocketBuffer::~SocketBuffer()
+{
+    for (auto & it : _buf_list) {
+        _mem_pool->Free(it.buf);
+        it.buf = nullptr;
+        it.write_index = 0;
+    }
+}
+
+uint32_t SocketBuffer::Size() const
+{
+    return _buf_size;
+}
+
+bool SocketBuffer::Append(const char* buf, uint32_t len)
+{
+    if (_buf_list.empty()) {
+        _buf_list.push_back(Block());
+    }
+    Block *blk = &_buf_list.back();
+
+    uint32_t copy_len = len;
+    const char *ptr = buf;
+    while (copy_len > 0) {
+        uint32_t cycle_len = copy_len > _block_size - blk->write_index ? _block_size - blk->write_index: copy_len;
+        if (!blk->buf) {
+            blk->buf = (char*)_mem_pool->Malloc(_block_size);
+        }
+
+        memcpy(blk->buf + blk->write_index, ptr, cycle_len);
+        blk->write_index += cycle_len;
+
+        copy_len -= cycle_len;
+        ptr += cycle_len;
+
+        if (copy_len > 0) {
+            _buf_list.push_back(Block());
+            blk = &_buf_list.back();
+        }
+    }
+
+    _buf_size += len;
+    return true;
+}
+
+uint32_t SocketBuffer::Get(char* buf, uint32_t len) const
+{
+    std::list<Block>::const_iterator it = _buf_list.begin();
+
+    char *ptr = buf;
+    uint32_t copy_len = len > _buf_size ? _buf_size : len;
+    uint32_t left = copy_len;
+    while (left > 0 && it != _buf_list.end() && it->buf != nullptr) {
+        uint32_t cycle_len = left > it->write_index - it->read_index ? it->write_index - it->read_index : left;
+
+        memcpy(ptr, it->buf + it->read_index, cycle_len);
+        ptr += cycle_len;
+        left -= cycle_len;
+
+        ++it;
+    }
+
+    return copy_len;
+}
+
+uint32_t SocketBuffer::Take(char* buf, uint32_t len)
+{
+    std::list<Block>::iterator it = _buf_list.begin();
+    std::list<Block>::iterator next;
+
+    char *ptr = buf;
+    uint32_t copy_len = len > _buf_size ? _buf_size : len;
+    uint32_t left = copy_len;
+    while (left > 0 && it != _buf_list.end() && it->buf != nullptr) {
+        next = it;
+        ++next;
+
+        uint32_t cycle_len = left > it->write_index - it->read_index ? it->write_index - it->read_index : left;
+
+        memcpy(ptr, it->buf + it->read_index, cycle_len);
+        ptr += cycle_len;
+        left -= cycle_len;
+
+        it->read_index += cycle_len;
+        if (it->write_index < _block_size) {
+            _mem_pool->Free(it->buf);
+            it->buf = nullptr;
+            _buf_list.erase(it);
+        }
+
+        it = next;
+    }
+
+    _buf_size -= copy_len;
+    return copy_len;
+}
+
+// TODO to test
+int SocketBuffer::SendToSocket(int sock_fd)
+{
+    std::list<Block>::iterator it = _buf_list.begin();
+    std::list<Block>::iterator next;
+
+    int total_send = 0;
+    while (it != _buf_list.end() && it->buf != nullptr) {
+        next = it;
+        ++next;
+
+        uint32_t to_send = it->write_index - it->read_index;
+        int isend = write(sock_fd, it->buf, to_send);
+        if (isend < 0) {
+            // TODO 来标明不同的socket错误,有些是网络原因导致,如EAGAIN,有些是socket出错
+            return isend;
+        }
+
+        it->read_index += (uint32_t)isend;
+        total_send += isend;
+
+        if ((uint32_t)isend != to_send) {
+            return total_send;
+        }
+
+        _mem_pool->Free(it->buf);
+        it->buf = nullptr;
+        _buf_list.erase(it);
+
+        it = next;
+    }
+
+    return total_send;
+}
+
+} /* namespace ef */
+
+
+#if 0
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+const char * str = "abcdefghijklmnopqrstuvwxyz";
+
+void test_get() {
+    ef::MemPool pool;
+    ef::SocketBuffer buffer(&pool, 5);
+    uint32_t len = strlen(str);
+    buffer.Append(str, len);
+
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    buffer.Append(str, len);
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    char buf[1024];
+    memset(buf, 0, sizeof(buf));
+    uint32_t size = buffer.Get(buf, 3);
+    cout << "Get 3 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 5);
+    cout << "Get 5 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 7);
+    cout << "Get 7 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 10);
+    cout << "Get 10 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 13);
+    cout << "Get 13 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 33);
+    cout << "Get 33 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 1024);
+    cout << "Get 1024 bytes, returned get " << size  << " content : " << buf << endl;
+}
+
+void test_take() {
+    ef::MemPool pool;
+    ef::SocketBuffer buffer(&pool, 5);
+    uint32_t len = strlen(str);
+    buffer.Append(str, len);
+
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    buffer.Append(str, len);
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    char buf[1024];
+    memset(buf, 0, sizeof(buf));
+    uint32_t size = buffer.Take(buf, 3);
+    cout << "Take 3 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 5);
+    cout << "Take 5 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 7);
+    cout << "Take 7 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 10);
+    cout << "Take 10 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 13);
+    cout << "Take 13 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 33);
+    cout << "Take 33 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 1024);
+    cout << "Take 1024 bytes, returned take " << size  << " content : " << buf << endl;
+}
+
+void test_append_get_and_take() {
+    ef::MemPool pool;
+    ef::SocketBuffer buffer(&pool, 5);
+    uint32_t len = strlen(str);
+    buffer.Append(str, len);
+
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    buffer.Append(str, len);
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    char buf[1024];
+    memset(buf, 0, sizeof(buf));
+    uint32_t size = buffer.Take(buf, 3);
+    cout << "Take 3 bytes, returned take" << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 20);
+    cout << "Get 20 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 13);
+    cout << "Take 13 bytes, returned take " << size  << " content : " << buf << endl;
+
+    buffer.Append(str, len);
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 300);
+    cout << "Get 300 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 33);
+    cout << "Take 33 bytes, returned take " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 30);
+    cout << "Get 30 bytes, returned get " << size  << " content : " << buf << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 1024);
+    cout << "Take 1024 bytes, returned take " << size  << " content : " << buf << endl;
+
+    buffer.Append(str, len);
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Take(buf, 10);
+    cout << "Take 10 bytes, returned take " << size  << " content : " << buf << endl;
+
+    cout << "buffer size after append: " << buffer.Size() << endl;
+
+    memset(buf, 0, sizeof(buf));
+    size = buffer.Get(buf, 300);
+    cout << "Get 300 bytes, returned get " << size  << " content : " << buf << endl;
+
+    cout << "buffer size after append: " << buffer.Size() << endl;
+}
+
+int main(int argc, char **argv)
+{
+    cout << "test append/get " << endl;
+    test_get();
+
+    cout << endl << endl;
+    cout << "test append/get " << endl;
+    test_take();
+
+    cout << endl << endl;
+    cout << "test append/get/take " << endl;
+    test_append_get_and_take();
+    return 0;
+}
+
+#endif
---
+++ common/socket_buffer.h
@@ -0,0 +1,46 @@
+/*
+ * Author: number
+ * Date  : Nov 18, 2015
+ */
+
+#ifndef __COMMON_SOCKET_BUFFER_H_H___
+#define __COMMON_SOCKET_BUFFER_H_H___
+
+#include <cstdint>
+#include <list>
+
+#define DEFAULT_BUFFER_BLOCK_SIZE 1024
+
+namespace ef {
+
+class MemPool;
+
+class SocketBuffer {
+public:
+    SocketBuffer(MemPool *pool, uint32_t block_size = DEFAULT_BUFFER_BLOCK_SIZE);
+    virtual ~SocketBuffer();
+
+    uint32_t Size() const;
+    bool Append(const char *buf, uint32_t len);
+    uint32_t Get(char *buf, uint32_t len) const;
+    uint32_t Take(char *buf, uint32_t len);
+    int SendToSocket(int sock_fd);
+
+private:
+    struct Block {
+        char * buf = nullptr;
+        uint32_t read_index = 0;
+        uint32_t write_index = 0;
+    };
+
+private:
+    std::list<Block> _buf_list;
+    uint32_t _block_size;
+    uint32_t _buf_size;
+
+    MemPool *_mem_pool;
+};
+
+} /* namespace ef */
+
+#endif /* __COMMON_SOCKET_BUFFER_H__ */
