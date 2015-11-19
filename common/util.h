083af90f10939be5294d4d104e3bc15dbe9c1e23
keynumber <keynumber@yeah.net>
2015-11-20 00:20:11 +0800

    add util

---
+++ common/util.h
@@ -0,0 +1,17 @@
+/*
+ * Author: number
+ * Date  : Nov 19, 2015
+ */
+
+#ifndef __COMMON_UTIL_H_H___
+#define __COMMON_UTIL_H_H___
+
+#include <string>
+
+namespace ef {
+
+std::string JoinPath(const std::string &p1, const std::string &p2);
+
+} /* namespace ef */
+
+#endif /* __COMMON_UTIL_H__ */
