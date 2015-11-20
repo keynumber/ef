b7b54a73ec34bddbe39f6e032df82acec3296f84
keynumber <keynumber@yeah.net>
2015-11-20 16:26:02 +0800

    add safe_strerror

--- common/io_wrapper.cpp
+++ common/io_wrapper.cpp
@@ -4,6 +4,8 @@
 
 #include <unistd.h>
 
+#include <string>
+
 #include "macro.h"
 
 ssize_t safe_read(int fd, void *buf, size_t count)
@@ -33,3 +35,15 @@ int safe_close(int fd)
     return ret;
 }
 
+std::string safe_strerror(int errorno)
+{
+    const int buf_len = 256;
+    char buf[buf_len];
+#if (_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+    strerror_r(errorno, buf, buf_len);
+    return std::string(buf);
+#else
+    const char * error = strerror_r(errorno, buf, buf_len);
+    return std::string(error);
+#endif
+}
