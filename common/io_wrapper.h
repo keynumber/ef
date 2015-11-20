2d8c38d9e9865a32679c8d7541b45a4e0e28c86a
keynumber <keynumber@yeah.net>
2015-11-20 16:27:01 +0800

    add safe_strerror

--- common/io_wrapper.h
+++ common/io_wrapper.h
@@ -1,5 +1,9 @@
 #include <sys/types.h>
 
+#include <string>
+
 ssize_t safe_read(int fd, void *buf, size_t count);
 ssize_t safe_write(int fd, const void *buf, size_t count);
 int     safe_close(int fd);
+
+std::string safe_strerror(int errorno)
