cbdcc506e7b7089da138e5b2149403c769b26e08
keynumber <keynumber@yeah.net>
2015-11-20 16:41:13 +0800

    Update io_wrapper.h

--- common/io_wrapper.h
+++ common/io_wrapper.h
@@ -1,9 +1,7 @@
 #include <sys/types.h>
 
-#include <string>
-
 ssize_t safe_read(int fd, void *buf, size_t count);
 ssize_t safe_write(int fd, const void *buf, size_t count);
 int     safe_close(int fd);
 
-std::string safe_strerror(int errorno)
+char * safe_strerror(int errorno, char *buf, size_t buflen);
